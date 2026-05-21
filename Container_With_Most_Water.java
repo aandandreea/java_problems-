6b0bca9a985c8c85f4a676d09091475113800898
Andreea <126575169+aandandreea@users.noreply.github.com>
2026-05-21 14:12:57 +0300

    Update Container_With_Most_Water.java

--- Container_With_Most_Water.java
+++ Container_With_Most_Water.java
@@ -30,3 +30,25 @@ class Solution {
         return maxwater;
     }
 }
+
+// another solution
+
+class Solution {
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int maxLength = 0;
+        while(left < right){
+            int currentLength = (right - left) * Math.min(heights[left],heights[right]);
+           
+            maxLength = Math.max(maxLength,currentLength);
+
+            if(heights[left] < heights[right]){
+                left++;
+            } else right--;
+        }
+        return maxLength;
+    }
+}
+
+
